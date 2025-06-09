19f907dc8fac66ad5cc9a8f3027feef8dd267992
billzi <ydfq8888@gmail.com>
2025-06-09 09:37:28 -0500

    Add files via upload

---
+++ sort/stable_quicksort.py
@@ -0,0 +1,25 @@
+import random
+
+
+def qs(nums):
+
+    if len(nums) < 2: return nums
+
+    pivot = random.choice(nums)
+    left, mid, right = [], [], []
+    for i in nums:
+        if i < pivot: left.append(i)
+        elif i > pivot: right.append(i)
+        else: mid.append(i)
+
+    return qs(left) + mid + qs(right)
+
+
+if __name__ == '__main__':
+
+    nums = list(range(20))
+    random.shuffle(nums)
+
+    print(nums)
+    print(qs(nums))
+    
